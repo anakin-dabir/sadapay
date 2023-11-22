1dd13794535d806231becf890f31363990500403
anakin-dabir <anakindabir@gmail.com>
2023-11-22 11:56:26 +0500

    modified:   public/service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,55 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('my-cache').then(cache => {
-      console.log('cached');
-      return cache.addAll(['/']);
+const cacheName = 'my-news-app-cache';
+const dbName = 'my-news-app-db';
+const storeName = 'user-inputs';
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName).then(cache => {
+      return cache.match(event.request).then(response => {
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(networkResponse => {
+          if (networkResponse && networkResponse.status === 200) {
+            const clonedResponse = networkResponse.clone();
+            cache.put(event.request, clonedResponse);
+          }
+
+          return networkResponse;
+        });
+      });
     })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      console.log('cach fetched');
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('sync', event => {
+  if (event.tag === 'sync-user-inputs') {
+    event.waitUntil(syncUserInputs());
+  }
 });
+
+function syncUserInputs() {
+  return caches.open(cacheName).then(cache => {
+    return cache.keys().then(keys => {
+      const requests = keys.filter(key => key.url.includes('/user-inputs/'));
+
+      return Promise.all(
+        requests.map(request => {
+          return cache.match(request).then(response => {
+            if (response) {
+              return response.text().then(data => {
+                return fetch('/sync-user-inputs', {
+                  method: 'POST',
+                  body: data,
+                }).then(() => {
+                  return cache.delete(request);
+                });
+              });
+            }
+          });
+        })
+      );
+    });
+  });
+}
